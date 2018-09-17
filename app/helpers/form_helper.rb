efa2353a30c450e11a6a8d0b68c1e115e5fd84b1
Alex Pate <alex@alexpate.uk>
2018-09-17 20:19:17 +0100

    Move helpers to app/helpers

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+module FormHelper
+  class ActionView::Helpers::FormBuilder
+    include ActionView::Helpers::FormTagHelper
+    include ActionView::Helpers::FormOptionsHelper
+    include ActionView::Context
+
+    UI_OPTIONS = [:hint, :label, :size]
+    SELECT_OPTIONS = [:prompt, :include_blank]
+
+    def ui_text_field(name, options = {})
+      options = set_classname(name, options)
+      ui_field_wrapper(name, text_field(name, extract_input_options(options)), options)
+    end
+
+    def ui_email_field(name, options = {})
+      options = set_classname(name, options)
+      ui_field_wrapper(name, email_field(name, extract_input_options(options)), options)
+    end
+
+    def ui_telephone_field(name, options = {})
+      options = set_classname(name, options)
+      ui_field_wrapper(name, telephone_field(name, extract_input_options(options)), options)
+    end
+
+    def ui_select_field(name, content, options = {})
+      options = set_classname(name, options, 'select')
+      ui_field_wrapper(name, select(name, content, only_select_options(options), extract_select_options(options)), options)
+    end
+
+    def ui_date_field(name, options = {})
+      options = set_classname(name, options)
+      ui_field_wrapper(name, date_field(name, extract_input_options(options)), options)
+    end
+
+    def ui_field_wrapper(name, content, options = {})
+      errors = error_messages(name)
+      inline_help = hint_tag(options[:hint])
+      inline_errors = error_tag(name, errors)
+      inline_help = inline_help_tag(inline_help, inline_errors)
+      children = label(name, options[:label], class: "ui-field-label") + content + inline_help
+      is_required = !!options[:required]
+
+      content_tag(:div, children, class: "ui-field#{is_required ? " ui-field--required" : ""}")
+    end
+
+    private
+
+      def error_tag(name, errors)
+        errors = Array.wrap(errors)
+        return nil if errors.empty?
+        human_attr = object.class.human_attribute_name(name)
+        return content_tag(:div, "#{human_attr} #{errors.first}".humanize, class: "ui-field-validation") if errors.length == 1
+        content_tag(:div, class: "ui-field-validations") do
+          errors.each { |e| concat content_tag(:div, "#{human_attr} #{e}".humanize, class: "ui-field-validation") }
+        end
+      end
+
+      def hint_tag(hint)
+        return nil if hint.nil?
+        content_tag(:div, hint, class: "ui-field-hint")
+      end
+
+      def inline_help_tag(help, errors)
+        errors || help || nil
+      end
+
+      def object
+        @object.presence || @template.instance_variable_get("@#{@object_name}")
+      end
+
+      def error_messages(name)
+        object.errors[name]
+      end
+
+      def set_classname(name, options = {}, tag = 'input')
+        errors = error_messages(name)
+        options[:class] = options[:class] ? "ui-#{tag} #{options[:class]}" : "ui-#{tag}"
+        options[:class] = "#{options[:class]} ui-input--#{options[:size] || 'medium'}"
+        options[:class] = options[:class] + " ui-input--invalid" if errors.any?
+        options
+      end
+
+      def extract_input_options(options)
+        options.except(*UI_OPTIONS)
+      end
+
+      def extract_select_options(options)
+        extract_input_options(options).except(*SELECT_OPTIONS)
+      end
+
+      def only_select_options(options)
+        options.slice(*SELECT_OPTIONS)
+      end
+  end
+end
