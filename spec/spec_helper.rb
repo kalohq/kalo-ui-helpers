6b7dfc5c7335e53f594070194c55feb02f7f1873
Alex Pate <alex@alexpate.uk>
2018-09-17 20:50:25 +0100

    Cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,12 @@
-# Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
+ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), "../")
+
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 
 require "rspec/rails"
 require "factory_bot_rails"
 
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), "../")
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
